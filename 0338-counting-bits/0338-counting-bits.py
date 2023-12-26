5445691b029e5098f868caa092e05824d6af8a6f
lohiyalalit <63835363+lohiyalalit@users.noreply.github.com>
2023-12-26 20:54:11 +0530

    Time: 57 ms (98.15%), Space: 24.1 MB (5.76%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        if n == 0:
+            return [0]
+        
+        dp = [0 for i in range(n+1)]
+        dp[1] = 1
+        
+        
+        for i in range(2,n+1):
+            
+            if i%2==0:
+                dp[i] = dp[i//2]
+                
+            else:
+                dp[i] = dp[i//2] + 1
+            
+        
+        return dp
