270d2f2ec8aebeb78b247e60e29200f5e65c4c54
lohiyalalit <63835363+lohiyalalit@users.noreply.github.com>
2023-12-27 17:56:27 +0530

    Time: 41 ms (39.24%), Space: 17.2 MB (5.28%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        line = [1]
+        n = rowIndex
+        for k in range(n):
+            line.append(int(line[k] * (n-k) / (k+1)))
+        
+        return line
